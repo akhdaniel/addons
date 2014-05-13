6a5ffbecdeeac2eb20d054ae648831c692d8d9f4
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2014-05-13 10:38:38 +0000

    import

---
+++ addons/vit_sync_slave/account_move.py
@@ -0,0 +1,20 @@
+import logging
+from osv import osv, fields
+import time
+from datetime import datetime
+from openerp import netsvc
+import openerp.tools
+from openerp.tools.translate import _
+
+_logger = logging.getLogger(__name__)
+
+class account_move(osv.osv):
+	_inherit = "account.move"
+	_columns = {
+		'is_exported'	: fields.boolean('Is Exported?'),
+		'exported_date'	: fields.datetime('Export Date')
+	}
+	_defaults = {
+		'is_exported': False 
+	}
+account_move()
