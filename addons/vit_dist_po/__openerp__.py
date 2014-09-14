77db82fb74c7ea0f5ed4979df0f0c3dc46db175b
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2014-09-14 22:34:15 +0000

    commit

---
+++ addons/vit_dist_po/__openerp__.py
@@ -0,0 +1,15 @@
+{
+	"name": "PO Distributor", 
+	"version": "1.0", 
+	"depends": ["base","purchase"], 
+	"author": "vitraining.com", 
+	"category": "Purchasing", 
+	"description": """\
+Fitur PO untuk Distributor
+- 
+""",
+	"data": [
+		],
+	"installable": True,
+	"auto_install": False,
+}
---
+++ addons/vit_dist_po/po.py
@@ -0,0 +1,8 @@
+from openerp.osv import fields, osv
+
+class purchase_order(osv.osv):
+	_name    = "purchase.order"
+	_inherit = "purchase.order"
+
+	_columns = {
+	}
