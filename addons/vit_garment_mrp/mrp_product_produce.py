75ece0cf69e2bce266e58f371342a247d667b9bb
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2014-09-30 12:35:46 +0000

    process on change

--- addons/vit_garment_mrp/mrp_product_produce.py
+++ addons/vit_garment_mrp/mrp_product_produce.py
@@ -30,11 +30,29 @@ class mrp_product_produce(osv.osv_memory):
         if context.get('active_model') == 'mrp.production':
             mrp = self.pool.get('mrp.production').browse(cr, uid, context.get('active_id', False), context=context)
             return mrp.product_id.id
-
         return False
 
+    def _get_product_grade_b_id(self, cr, uid, context):
+        if context.get('active_model') == 'mrp.production':
+            mrp = self.pool.get('mrp.production').browse(cr, uid, context.get('active_id', False), context=context)
+            p = self.pool.get('product.product').search(cr, uid, 
+                [('name','=', "%s - B" %(mrp.product_id.name) )], context=context)
+            if not p:
+                return False
+            return p[0] or False 
+        return False
+
+    def _get_product_waste_id(self, cr, uid, context):
+        p = self.pool.get('product.product').search(cr, uid, 
+            [('categ_id','ilike', "waste" )], context=context)
+        if not p:
+            return False
+        return p[0] or False 
+
     _defaults = {
-        'product_id' : _get_product_id
+        'product_id'         : _get_product_id,
+        'product_grade_b_id' : _get_product_grade_b_id,
+        'product_waste_id'   : _get_product_waste_id
     }
 
     def onchange_qty(self, cr, uid, ids, product_qty, context=None):
@@ -44,6 +62,17 @@ class mrp_product_produce(osv.osv_memory):
         results = {
             'value' : {
                 'product_grade_b_qty' : old_qty - product_qty
+            }
+        }
+        return results
+
+    def onchange_grade_b_qty(self, cr, uid, ids, product_qty, grade_b_qty, context=None):
+        mrp_id  = context.get('active_id')
+        mrp     = self.pool.get('mrp.production').browse(cr, uid, mrp_id, context=context)
+        old_qty = mrp.move_created_ids[0].product_qty 
+        results = {
+            'value' : {
+                'product_waste_qty'   : old_qty - product_qty - grade_b_qty
             }
         }
         return results
@@ -62,20 +91,27 @@ class mrp_product_produce(osv.osv_memory):
             mrp     = mrp_obj.browse(cr, uid, mrp_id, context=context)
 
             ##########################################################################
-            # cek total old product_qty = product_qty + grade_b + waste
-            # kalau data.product_grade_b_qty + data.product_waste_qty > 0
-            # kalau ya, proses
+            # hanya proses kalau data.product_grade_b_qty + data.product_waste_qty > 0
             ##########################################################################
             old_qty = mrp.move_created_ids[0].product_qty 
             if data.product_grade_b_qty + data.product_waste_qty > 0:
 
                 ##########################################################################
                 # validasi product dan qty
+                # cek total old product_qty = product_qty + grade_b + waste
                 ##########################################################################
                 if old_qty != data.product_qty + data.product_grade_b_qty + data.product_waste_qty:
                     raise osv.except_osv(_('Error'),_("Total Qty of Production Order must be the same as Product, Grade B, and Waste Quantity") ) 
+                
+                ##########################################################################
+                # validasi nggak ada product grade b tapi ada qty
+                ##########################################################################
                 if data.product_grade_b_qty > 0 and not data.product_grade_b_id:
                     raise osv.except_osv(_('Error'),_("Please select Grade B Product") ) 
+                
+                ##########################################################################
+                # validasi nggak ada product waste tapi ada qty
+                ##########################################################################
                 if data.product_waste_qty > 0 and not data.product_waste_id:
                     raise osv.except_osv(_('Error'),_("Please select Waste Product") ) 
 
@@ -130,14 +166,34 @@ class mrp_product_produce(osv.osv_memory):
                     id = sm_obj.create(cr, uid, sm_data, context=context)
                     sm_obj.action_done(cr, uid, [id], context=context)
 
+
+
+        res = super(mrp_product_produce, self).do_produce(cr, uid, ids, context=context)
+
+
+        ##########################################################################
+        # proses sisa produk asli
+        ##########################################################################
+        if data.mode == 'consume_produce':
+            if data.product_grade_b_qty + data.product_waste_qty >0:
+
                 ##########################################################################
                 # proses sisa produk asli, delete dari product to produce
                 ##########################################################################
-                cr.execute("delete from stock_move where name='%s' and state<>'done'" % (mrp.name))
-                # ids = sm_obj.search(cr, uid, [('name','=',mrp.name)], context=context)
-                # sm_obj.unlink(cr, uid, ids , context=context)
+                cr.execute("delete from stock_move where production_id=%d and state<>'done' and product_id=%d" 
+                    % (mrp.id, mrp.product_id.id))
 
-        res = super(mrp_product_produce, self).do_produce(cr, uid, ids, context=context)
+                ##########################################################################
+                # proses sisa bahan asli, delete dari product to consume
+                ##########################################################################
+                cr.execute("delete from stock_move where name='%s' and state<>'done'" 
+                    % (mrp.name))        
+
+                ##########################################################################
+                # update state mrp jadi done
+                ##########################################################################
+                cr.execute("update mrp_production set state='done' where id=%d" % (mrp.id) )
+
         return res 
 
 
