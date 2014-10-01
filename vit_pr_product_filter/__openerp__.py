02e9b1a8b0e3e33c8258b2d62051be4c0c1f04da
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2014-10-01 13:18:55 +0000

    http://svn.vitraining.com:8080/repos/addons/vit_pr_product_filter

---
+++ vit_pr_product_filter/__openerp__.py
@@ -0,0 +1,25 @@
+{
+	"name": "Information", 
+	"version": "1.0", 
+	"depends": [
+		"base",
+		"board",
+		"purchase_requisition_multi_supplier",
+	], 
+	"author": "vitraining.com", 
+	"category": "Purchase", 
+	"description": """\
+
+Features
+==============
+
+* filter product selection on purchase requisition form
+* only product which can be purchase appears on the form
+
+""",
+	"data": [
+		"pr.xml", 
+		],
+	"installable": True,
+	"auto_install": False,
+}
