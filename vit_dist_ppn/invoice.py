d1b6504e821499f4d971b147cd39e6b4263af266
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2014-10-05 22:57:17 +0000

    ppn espt

--- vit_dist_ppn/invoice.py
+++ vit_dist_ppn/invoice.py
@@ -4,6 +4,12 @@ class invoice(osv.Model):
 	_name    = 'account.invoice'
 	_inherit = 'account.invoice'
 	_columns = {
-		'tax_number' : fields.char('Tax Number')
+		'tax_number' : fields.char('Tax Number'),
+		'harga_jual' : fields.boolean('Harga Jual?'),
+		'penggantian': fields.boolean('Penggantian?'),
+		'uang_muka'  : fields.boolean('Uang Muka?'),
+		'termijn'    : fields.boolean('Termijn?'),
+		'espt_export': fields.boolean('e-SPT Export?'),
+		'espt_export_date': fields.date('e-SPT Export Date')
 	}
 	
