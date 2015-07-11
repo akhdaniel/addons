637ab497036005be9612dfdef0173f30fb2af55b
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2015-07-11 22:43:49 +0000

    vit claim website

---
+++ addons/netpro/vit_claim_web/__openerp__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Claim Website",
+
+    'summary': """
+        Homepage untuk aplikasi insurance""",
+
+    'description': """
+Homepage untuk aplikasi insurance
+    """,
+
+    'author': "vitraining.com",
+    'website': "http://www.vitraining.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Website',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['website','website_blog','vit_policy', 'vit_member', 'vit_claim'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/pages.xml',
+
+    ],
+    'css':['static/src/css/*.css'],
+    'js':['static/src/js/*.js'],
+    'installable':True,
+}
