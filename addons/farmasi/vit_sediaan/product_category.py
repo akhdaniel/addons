206994ac2da295537f24a2cb47e4a1f659bbe950
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2015-08-18 13:56:20 +0000

    sediaan

---
+++ addons/farmasi/vit_sediaan/product_category.py
@@ -0,0 +1,22 @@
+from openerp import tools
+from openerp.osv import fields,osv
+import openerp.addons.decimal_precision as dp
+import time
+import logging
+from openerp.tools.translate import _
+
+_logger = logging.getLogger(__name__)
+
+class product_category(osv.osv):
+	_name 		= "product.category"
+	_inherit 	= "product.category"
+	_columns 	= {
+		'sediaan_id' : fields.many2one('vit.sediaan', 'Sediaan')
+	}
+
+class sediaan(osv.osv):
+	_name 		= "vit.sediaan"
+	_columns 	= {
+		'name' 	: fields.char("Sediaan"),
+		'code' 	: fields.char("Code"),
+	}
