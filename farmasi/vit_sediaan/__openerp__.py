0aa3c62dc5ea8d15160ba8abcfcdc8f2f36cc62c
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2015-08-18 13:56:20 +0000

    sediaan

---
+++ farmasi/vit_sediaan/__openerp__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Product Category SEdiaan',
+    'version': '1.0',
+    'category': 'Manufacture',
+    'description': """
+Fitur Modul
+==================================
+
+* tambah sediaan_id di master produk category
+
+       """,
+    'author': 'vitraining.com',
+    'depends': ['stock'],
+    'data': [
+        # 'product_views.xml',
+        # 'bom_views.xml',
+    ],
+    'test': [],    
+    'installable': True,
+    'active': False,
+    'certificate':''   
+}
