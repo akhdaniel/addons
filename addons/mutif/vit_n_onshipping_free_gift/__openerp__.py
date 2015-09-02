cdb41a7ba41a795259b0f075d5afefec101d5f96
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2015-09-02 06:09:42 +0000

    ok

---
+++ addons/mutif/vit_n_onshipping_free_gift/__openerp__.py
@@ -0,0 +1,24 @@
+{
+	"name": "Add Free Gift On Shipping When Create Invoice", 
+	"version": "1.0", 
+	"depends": [
+		"base",
+		"stock",
+		"vit_sale_gift"	
+	], 
+	"author": "vitraining", 
+	"category": "Warehouse", 
+	"description": """\
+
+Add
+==============
+
+* Add Free Gift On Shipping When Create Invoice
+
+""",
+	"data": [
+		"stock_invoice_onshipping_view.xml"
+	],
+	"installable": True,
+	"auto_install": False,
+}
