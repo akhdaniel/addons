20da1804729899916b3e7c6184c175f78489146b
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2015-09-02 06:07:56 +0000

    ok2

---
+++ mutif/vit_n_add_qty_on_wizard_do/__openerp__.py
@@ -0,0 +1,24 @@
+{
+	"name": "add qty on wizard do", 
+	"version": "1.0", 
+	"depends": [
+		"base",
+		"stock"	
+	], 
+	"author": "vitraining", 
+	"category": "Warehouse", 
+	"description": """\
+
+Add
+==============
+
+* vit_n_add_qty_on_wizard_do
+* Menambah field Jumlah Quantity di Wizard Receive Product dan Transfer Product
+
+""",
+	"data": [
+		"deliver.xml"
+	],
+	"installable": True,
+	"auto_install": False,
+}
