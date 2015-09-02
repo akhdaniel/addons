616c9ec75117f742a3b9f9055301eddce1b4b761
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2015-09-02 06:05:31 +0000

    ok

---
+++ mutif/vit_n_cutting_order/__openerp__.py
@@ -0,0 +1,30 @@
+{
+    'version': '0.1',
+    'name': 'Cutting Order',
+    'depends': ['base','report_webkit','product','procurement', 'stock', 'resource', 'purchase','process','vit_garment_custom_product'],
+    'author'  :'vitraining.com',
+    'category': 'Manufacturing',
+    'description': """
+        Cutting Order
+""",
+    'installable':True,
+    'data': [
+        'menu.xml',
+        'cutting_order.xml',
+        'makloon_order.xml',
+        'master_type.xml',
+        'product.xml',
+        'mrp_view.xml',
+        # 'report/spk.xml',
+        'report/spk_webkit.xml',
+        'report/aksesoris_webkit.xml',
+        'report/makloon_webkit.xml',
+        'report/aksesoris_webkit_1.xml',
+        'conf_cutting.xml',
+        'stock_move.xml',
+        'data.xml'
+
+    ],
+}
+
+
