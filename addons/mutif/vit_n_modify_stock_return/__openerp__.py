a4fc96230abc3baa7a7d54ad65470a6e386c09bf
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2015-09-02 06:09:53 +0000

    ok

---
+++ addons/mutif/vit_n_modify_stock_return/__openerp__.py
@@ -0,0 +1,24 @@
+{
+	"name": "Vit Modify Stock return", 
+	"version": "1.0", 
+	"depends": [
+		"base",
+		"stock"	
+	], 
+	"author": "vitraining", 
+	"category": "Warehouse", 
+	"description": """\
+
+Modify
+==============
+
+* Quantity Default Field On Stock Return Picking = 0
+* Add Field Order and Modify Button With Order Return Product
+
+""",
+	"data": [
+		"stock_return_picking_view2.xml",
+	],
+	"installable": True,
+	"auto_install": False,
+}
