62c94511d8de66a8fbd4d4bfc6568b4591f66473
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2015-10-21 20:27:13 +0000

    vit_bom_lines

---
+++ farmasi/vit_bom_lines/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'version': '0.1',
+    'name': 'BOM Lines menus',
+    'depends': ['base','mrp','vit_pharmacy_machine_hour','vit_bom_component_type'],
+    'author'  :'vitraining.com',
+    'category': 'Manufacturing',
+    'description': """
+        add BOM lines menu supaya bsa import bom.line.csv
+""",
+    'installable':True,
+    'data': [
+        'bom.xml',
+    ],
+}
