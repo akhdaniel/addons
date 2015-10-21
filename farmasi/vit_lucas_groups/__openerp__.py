14e1ee3090403af536466b10b2795c8531ea5dc4
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2015-10-21 20:31:27 +0000

    vit_lucas_groups

---
+++ farmasi/vit_lucas_groups/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Groups user Lucas dan Marin",
+    'summary': """
+        Groups user Lucas dan Marin""",
+    'description': """
+        Groups user Lucas dan Marin
+    """,
+    'author': "akhmad.daniel@gmail.com",
+    'website': "http://www.vitraining.com",
+    'category': 'manufacturing',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'security/groups.xml',
+    ],
+    'demo': [
+    ],
+}
