e99395db5af35ca212c1e9e7a0c90a923c0cc59a
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2015-12-16 04:53:03 +0000

    menu cal mo

---
+++ addons/farmasi/vit_menu_wps_cal/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+##############################################################################
+
+{
+    'name': 'MO Calendar modul under Pharmacy',
+    'description': "",
+    'version': '0.1',
+    'depends': ['base','mrp','mrp','vit_pharmacy_manufacture', 'vit_batch_number_in_mo'],
+    'author': 'vitraining.com',
+    'category': 'mrp',
+    'url': 'http://www.vitraining.com/',
+    'data': [ 
+        
+        'menu.xml',
+        
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
