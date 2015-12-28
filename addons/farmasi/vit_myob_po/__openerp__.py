e2dd51fd51df3127b8db693225fd44f28ca30d43
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2015-12-28 07:47:15 +0000

    myob

---
+++ addons/farmasi/vit_myob_po/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'PO export to MYOB',
+    'version': '1.0',
+    'category': 'Purchase',
+    'description': """
+PO export to MYOB       """,
+    'author': 'akhmad.daniel@gmail.com',
+    'depends': ['purchase'],
+    'data': [
+        'po.xml'
+    ],
+    'test': [],    
+    'installable': True,
+    'active': False,
+    'certificate':''   
+}
