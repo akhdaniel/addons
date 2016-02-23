470e18b7dce02bff85a369cd8a78048975cfedab
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2016-02-23 04:33:12 +0000

    http://svn.vitraining.com:8080/repos/addons/vit_user_signature

---
+++ addons/vit_user_signature/__openerp__.py
@@ -0,0 +1,23 @@
+{
+	"name": "Information", 
+	"version": "1.0", 
+	"depends": [
+		"base",
+		"account"
+	], 
+	"author": "Author Name", 
+	"category": "Education", 
+	"description": """\
+
+Manage
+==============
+
+* add user signature
+* modify user form
+* modify invoice report qweb
+
+""",
+	"data": ['user.xml', 'invoice_report.xml'],
+	"installable": True,
+	"auto_install": False,
+}
