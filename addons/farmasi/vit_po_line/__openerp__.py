ea2f3663be38db9c06f6c2701b59c415e870be32
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2016-02-26 02:54:50 +0000

    vit po line

---
+++ addons/farmasi/vit_po_line/__openerp__.py
@@ -0,0 +1,24 @@
+{
+	"name": "PO Line", 
+	"version": "1.0", 
+	"depends": [
+		"base",
+		"purchase"
+	], 
+	"author": "akhmad.daniel@gmail.com", 
+	"category": "Purchase", 
+	"description": """\
+
+Manage
+==========================================
+
+* created menu: Purchase > PO Lines 
+* this menu display PO Lines grouped by PO, Customer
+
+""",
+	"data": [
+		"menu.xml", 
+	],
+	"installable": True,
+	"auto_install": False,
+}
