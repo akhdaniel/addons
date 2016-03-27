8bc2a6eb9e8b22ed3a439103fc9f84607554b9e1
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2016-03-27 22:59:56 +0000

    http://svn.vitraining.com:8080/repos/addons/farmasi/vit_po_filter

---
+++ addons/farmasi/vit_po_filter/__openerp__.py
@@ -0,0 +1,22 @@
+{
+	"name": "PO Filter month", 
+	"version": "1.0", 
+	"depends": [
+		"purchase",
+	], 
+	"author": "akhmad.daniel@gmail.com", 
+	"category": "Purchase", 
+	"description": """\
+
+Manage
+======================================================================
+
+* add filter by month
+
+""",
+	"data": [
+		"po_filter.xml", 
+	],
+	"installable": True,
+	"auto_install": False,
+}
