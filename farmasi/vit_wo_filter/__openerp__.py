c52dd1400796b78348d4560c6ec2750d18ddd973
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2016-04-11 09:30:08 +0000

    http://svn.vitraining.com:8080/repos/addons/farmasi/vit_wo_filter

---
+++ farmasi/vit_wo_filter/__openerp__.py
@@ -0,0 +1,23 @@
+{
+	"name": "WO Filter Penimbangan - Draft", 
+	"version": "1.0", 
+	"depends": [
+		"mrp",
+		"mrp_operations",
+	], 
+	"author": "akhmad.daniel@gmail.com", 
+	"category": "Manufacture", 
+	"description": """\
+
+Manage
+======================================================================
+
+* add filter by penimbangan at draft state
+
+""",
+	"data": [
+		"wo_filter.xml", 
+	],
+	"installable": True,
+	"auto_install": False,
+}
