83fe6f9f5496b09fb96c288f57eee4bc70c3a2af
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2016-04-15 14:06:58 +0000

    text

--- addons/vit_reliance/php-example/tes.php
+++ addons/vit_reliance/php-example/tes.php
@@ -7,3 +7,11 @@ $password = "1";
 $common = ripcord::client("$url/xmlrpc/2/common");
 $uid = $common->authenticate($db, $username, $password, array());
 var_dump($uid);
+
+$models = ripcord::client($url . '/xmlrpc/2/object');
+$res = $models->execute_kw( $db, $uid, $password, 'res.partner', 'get_ls_stock', array(
+    array(
+        '000088888'
+    )
+));
+var_dump($res);
