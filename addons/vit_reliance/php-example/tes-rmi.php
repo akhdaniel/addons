55d3fb955f28258d9564ab44cd5b5f3f9f8449c9
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2016-04-23 08:56:07 +0000

    tes rmi

---
+++ addons/vit_reliance/php-example/tes-rmi.php
@@ -0,0 +1,30 @@
+<?php
+require_once('ripcord/ripcord.php');
+$url = "http://erp2.vitraining.com:8069";
+$db = "reliance";
+$username = "admin";
+$password = "1";
+$common = ripcord::client("$url/xmlrpc/2/common");
+$uid = $common->authenticate($db, $username, $password, array());
+var_dump($uid);
+
+echo "get member by reliance_id";
+$models = ripcord::client($url . '/xmlrpc/2/object');
+$method = 'get_rmi_customer';
+$reliance_id = '20';
+$res = $models->execute_kw( $db, $uid, $password, 'res.partner', $method , array(
+      $reliance_id 
+));
+var_dump($res);
+
+
+echo "get by sid";
+$models = ripcord::client($url . '/xmlrpc/2/object');
+$method = 'get_rmi_customer_by_sid';
+$sid= '100100000001';
+$res = $models->execute_kw( $db, $uid, $password, 'res.partner', $method , array(
+      $sid
+));
+var_dump($res);
+
+
