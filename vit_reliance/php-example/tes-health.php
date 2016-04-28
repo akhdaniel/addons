cb8084bf4cbc9b16c358c700073e8de681bd747f
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2016-04-28 03:13:26 +0000

    test api healt

--- vit_reliance/php-example/tes-health.php
+++ vit_reliance/php-example/tes-health.php
@@ -33,11 +33,28 @@ $res = $models->execute_kw( $db, $uid, $password, 'res.partner', $method , array
         $member_id
 ));
 var_dump($res);
-*/
 $models = ripcord::client($url . '/xmlrpc/2/object');
 $method = 'get_health_holder';
 $reliance_id= '011';
 $res = $models->execute_kw( $db, $uid, $password, 'res.partner', $method , array(
         $reliance_id
+));
+var_dump($res);
+*/
+
+$models = ripcord::client($url . '/xmlrpc/2/object');
+$method = 'get_health_limit';
+$reliance_id= '119';
+$res = $models->execute_kw( $db, $uid, $password, 'res.partner', $method , array(
+        $reliance_id
+));
+var_dump($res);
+
+$models = ripcord::client($url . '/xmlrpc/2/object');
+$method = 'get_health_limit_by_member_id';
+$member_id= '000284-1';
+$polis_no= '00014131100070802';
+$res = $models->execute_kw( $db, $uid, $password, 'res.partner', $method , array(
+        $polis_no,$member_id 
 ));
 var_dump($res);
