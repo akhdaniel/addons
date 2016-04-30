5aba12cb36aee359198b37e86d1fa13bbfedbbd0
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2016-04-30 22:39:32 +0000

    merge

--- vit_reliance/php-example/tes-merge.php
+++ vit_reliance/php-example/tes-merge.php
@@ -1,6 +1,6 @@
 <?php
 require_once('ripcord/ripcord.php');
-$url = "http://erp2.vitraining.com:8069";
+$url = "http://127.0.0.1:8069";
 $db = "reliance";
 $username = "admin";
 $password = "1";
@@ -9,11 +9,14 @@ $uid = $common->authenticate($db, $username, $password, array());
 var_dump($uid);
 
 $models = ripcord::client($url . '/xmlrpc/2/object');
-$method = '_merge';
-$partner_ids = [552,551];
-$dest_partner_id=551;
-$res = $models->execute_kw( $db, $uid, $password, 'res.partner', $method , [
-	$partner_ids,
-	$dest_partner_id
-]);
+$method = 'merge';
+$partner_ids = [551,552];
+$dest_partner_id=552;
+$res = $models->execute_kw( $db, $uid, $password, 'res.partner', $method , array(
+        $partner_ids, $dest_partner_id
+));
 var_dump($res);
+
+
+
+
