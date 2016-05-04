5f4a90156d88b75919c383c4414b9f0178cee912
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2016-05-04 05:31:21 +0000

    date

--- vit_reliance/model/import_ls.py
+++ vit_reliance/model/import_ls.py
@@ -171,7 +171,7 @@ class import_ls(osv.osv):
 			data.update( {'comment':'LS'})
 
 			date_birth = False
-			if import_ls.date_birth and import_ls.date_birth != "NULL":
+			if import_ls.date_birth:
 				try: 
 					date_birth = datetime.strptime(import_ls.date_birth, "%Y-%m-%d")
 				except ValueError:
@@ -256,7 +256,7 @@ class import_ls_cash(osv.osv):
 				###### cari existing Cash record ############
 				cid = cash.search(cr, uid, [('partner_id','=', pid[0] )], context=context)
 				date = False 
-				if import_cash.date and import_cash.date != "00:00.0":
+				if import_cash.date:
 					try: 
 						date = datetime.strptime(import_cash.date, "%Y-%m-%d")
 					except ValueError:
@@ -359,7 +359,7 @@ class import_ls_stock(osv.osv):
 				###### cari existing Cash record ############
 				cid = stock.search(cr, uid, [('partner_id','=', pid[0]),('stock_id','=',import_stock.stock_id)], context=context)
 				date = False 
-				if import_stock.date and import_stock.date != "00:00.0":
+				if import_stock.date:
 					try: 
 						date = datetime.strptime(import_stock.date, "%Y-%m-%d")
 					except ValueError:
