a353052026e2c24ac77a64873eadc2a4fad12568
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2016-05-12 23:14:59 +0000

    zip

--- vit_reliance/model/import_zip_ajri.py
+++ vit_reliance/model/import_zip_ajri.py
@@ -4,6 +4,9 @@ import openerp.addons.decimal_precision as dp
 import time
 import logging
 from openerp.tools.translate import _
+import ftp_utils as ftp
+import io
+import base64
 
 _logger = logging.getLogger(__name__)
 
@@ -12,12 +15,36 @@ class import_zip_ajri(osv.osv):
 	_columns 	= {
 		'date_import'			: 	fields.date("Import Date"),
 		'date_processed'		: 	fields.date("Processed Date"),
-		'ajri_customer_file'	:	fields.binary('AJRI Customer File (ZIP)'),
-		'is_imported' 			: 	fields.boolean("Imported to Partner?", select=1),
+		'zip_ajri_customer'		:	fields.binary('AJRI Customer File (ZIP)'),
+		'is_imported' 			: 	fields.boolean("Saved to AJRI FTP Folder?", select=1),
 		"notes"					:	fields.char("Notes"),
 	}
 	_defaults = {
 		'date_import'   : lambda *a : time.strftime("%Y-%m-%d") ,
 		'user_id'		: lambda obj, cr, uid, context: uid,
 	}
-	
+
+	def button_process(self, cr, uid, ids, context=None):
+		return self.actual_process(cr, uid, ids, context=context)
+
+	def actual_process(self, cr, uid, ids, context=None):
+
+		notes = ''
+
+		for import_zip_ajri in self.browse(cr, uid, ids, context=context):
+			cust_files = self.process_cust( cr, uid,  base64.decodestring(import_zip_ajri.zip_ajri_customer))
+
+		self.write(cr, uid, ids, {
+			'date_processed': time.strftime("%Y-%m-%d"),
+			'is_imported'	: True,
+			'notes'			: ",".join(cust_files)  
+		} , context=context)
+
+		return True
+
+	def process_cust(self, cr, uid,  zip_ajri_cust, context=None):
+		ftp_ajri_folder = self.pool.get('ir.config_parameter').get_param(cr, uid, 'ftp_ajri_folder')
+		ftp_utils = ftp.ftp_utils()
+		ajri_cust = io.BytesIO(zip_ajri_cust)
+		return ftp_utils.unzip(ajri_cust, ftp_ajri_folder )
+
