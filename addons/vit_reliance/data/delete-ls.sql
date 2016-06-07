698d7ea1e8a8132cdfa529e13a5220a50fc16177
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2016-06-07 22:33:41 +0000

    delete data

--- addons/vit_reliance/data/delete-ls.sql
+++ addons/vit_reliance/data/delete-ls.sql
@@ -1,6 +1,11 @@
 delete from reliance_partner_cash;
 delete from reliance_partner_stock;     
-delete from res_partner where comment like 'LS%';
-update reliance_import_ls SET  is_imported = false;
-update reliance_import_ls_stock SET  is_imported = false;
-update reliance_import_ls_cash SET  is_imported = false;
+delete from res_partner where initial_bu = 'LS';
+-- update reliance_import_ls SET  is_imported = false;
+-- update reliance_import_ls_stock SET  is_imported = false;
+-- update reliance_import_ls_cash SET  is_imported = false;
+
+delete from reliance_import_ls;
+delete from reliance_import_ls_stock;
+delete from reliance_import_ls_cash;
+
