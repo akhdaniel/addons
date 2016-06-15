c9efb95558a4b4886f7049640d8d68cd435eeeb9
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2016-06-15 23:52:18 +0000

    add

---
+++ farmasi/vit_daily_receive/__openerp__.py
@@ -0,0 +1,26 @@
+{
+	"name": "Daily Receive Report",
+	"version": "1.0", 
+	"depends": [
+		"stock"
+	], 
+	"author": "akhmad.daniel@gmail.com", 
+	"category": "Warehouse",
+	"description": """\
+
+Manage
+======================================================================
+
+* this is my academic information system module
+* created menu:
+* created object
+* created views
+* logic:
+
+""",
+	"data": [
+		"menu.xml", 
+	],
+	"installable": True,
+	"auto_install": False,
+}
