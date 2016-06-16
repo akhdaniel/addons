3a3e1dc008651e9c3de31bef1643408c8434c708
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2016-06-16 00:38:02 +0000

    wip

---
+++ farmasi/vit_wip/__openerp__.py
@@ -0,0 +1,26 @@
+{
+	"name": "WIP Report",
+	"version": "1.0", 
+	"depends": [
+		"stock",
+		"mrp","vit_batch_number_in_mo"
+	], 
+	"author": "akhmad.daniel@gmail.com", 
+	"category": "Manufacture",
+	"description": """\
+
+Manage
+======================================================================
+
+* created menu:
+* created object
+* created views
+* logic:
+
+""",
+	"data": [
+		"menu.xml", 
+	],
+	"installable": True,
+	"auto_install": False,
+}
