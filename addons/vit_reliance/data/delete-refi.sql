93cced4cd91eeca3aab95516ce68b7ae69f6d077
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2016-06-16 23:32:54 +0000

    delete refi

--- addons/vit_reliance/data/delete-refi.sql
+++ addons/vit_reliance/data/delete-refi.sql
@@ -1,7 +1,13 @@
 delete from reliance_refi_kontrak;
 delete from res_partner where comment like 'REFI%';
 update reliance_import_refi_partner SET  is_imported = false;
-update reliance_import_refi_pekerjaan import_ls_stock SET  is_imported = false;
+update reliance_import_refi_pekerjaan SET is_imported = false;
 update reliance_import_refi_keluarga SET  is_imported = false;
 update reliance_import_refi_statement SET  is_imported = false;
 update reliance_import_refi_kontrak SET  is_imported = false;
+
+delete from reliance_import_refi_partner;
+delete from reliance_import_refi_pekerjaan;
+delete from reliance_import_refi_keluarga;
+delete from reliance_import_refi_statement;
+delete from reliance_import_refi_kontrak;
