adb7c8f2d1ea8667ca5a37f81fd052c7cca25e48
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2016-06-20 03:41:44 +0000

    modul menambah parent di product

---
+++ farmasi/vit_product_parent/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+##############################################################################
+
+{
+    'name': 'Product Parent',
+    'description': """
+""",
+    'version': '0.1',
+    'depends': ['base','product'],
+    'author': 'vitraining.com',
+    'category': 'product',
+    'url': 'http://www.vitraining.com/',
+    'data': [ 
+        'parent_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
