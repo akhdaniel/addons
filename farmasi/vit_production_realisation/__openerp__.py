c6d13d5f1e46d581036af39f8460f349842d6c60
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2016-06-22 07:05:47 +0000

    vit_production_realisation

---
+++ farmasi/vit_production_realisation/__openerp__.py
@@ -0,0 +1,29 @@
+{
+	"name": "Production Realisation Report",
+	"version": "1.0", 
+	"depends": [
+		"stock",
+		"mrp",
+		"vit_batch_number_in_mo"
+	], 
+	"author": "akhmad.daniel@gmail.com", 
+	"category": "Manufacture",
+	"description": """\
+
+Manage
+======================================================================
+
+* created menu:
+* created object
+* created views
+* logic:
+
+""",
+	"data": [
+		"view/mrp_realisation.xml",
+		"menu.xml",
+
+	],
+	"installable": True,
+	"auto_install": False,
+}
