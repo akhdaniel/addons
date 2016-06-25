aa5bdf40a99bbd51399487332e20d37002a9c07a
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2016-06-25 10:03:20 +0000

    delete manfaat en

---
+++ addons/vit_reliance/data/delete-manfaat-en.sql
@@ -0,0 +1,2 @@
+delete from reliance_partner_health_limit where manfaat in('Maternity','Death Benefit','Glasses','Inpatient','Term Life','Pre and post Natal','Dental','Additional Benefit 2','Additional Benefit','Outpatient');
+
