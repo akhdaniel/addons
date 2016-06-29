5600fe91defebffa795f8ce70f6468d9c77cdb1c
prog1 <prog1@381544ba-743e-41a5-bf0d-221725b9d5af>
2016-06-29 07:45:25 +0000

    menambah field detail_available yang menjumlahkan qty_available dari anak2 product

--- farmasi/vit_product_parent/parent.py
+++ farmasi/vit_product_parent/parent.py
@@ -12,15 +12,9 @@ from openerp.addons.product import _common
 import logging
 _logger = logging.getLogger(__name__)
 
-class parent(osv.osv):
-	_inherit = 'product.product'
+class parent_template(osv.osv):
+	_inherit = 'product.template'
 
 	_columns = {
-				'parent_id':fields.many2one('product.product', 'Parent Product')
+				'parent_id':fields.many2one('product.template', 'Parent Product')
 	}
-
-
-	# def get_parent(self, cr, uid, ids):
-	# 	sql = 'UPDATE product_product pp set parent_id=(select id from product_product where default_code=substr(pp.default_code,1,6) limit 1) where length(default_code=10'
-
-	# return sql
